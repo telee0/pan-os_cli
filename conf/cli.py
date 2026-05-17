4f4ca74ca45bc7062c0b06c0c5b2fe70a8df40d1
Terence LEE <93152457+telee0@users.noreply.github.com>
2026-05-18 03:28:53 +0800

    Add files via upload

--- conf/cli.py
+++ conf/cli.py
@@ -1,5 +1,6 @@
 """
 
+pan-os_cli v2.1 [20260515]
 pan-os_cli v2.0 [20250420]
 
 Script to repeat CLI commands on PAN-OS over SSH
@@ -12,33 +13,35 @@ https://pexpect.readthedocs.io/en/stable/index.html
 """
 
 cf = {
-    'hostname': '192.168.1.1',   # host name or IP of the target device
-    'username': 'admin',         # sensitive and not exported, default admin
-    'password': '',              # sensitive and not exported, either here or through the env variable cf['passenv']
-    'passenv': 'PAPASS',         # name of the environment variable for the password
-    'prompt': r'.*>\s+',         # regex so prefixed with an 'r'
+    'hostname': '192.168.1.1',          # host name or IP of the target device
+    'username': 'admin',                # sensitive and not exported, default admin
+    'password': '',                     # sensitive and not exported, either here or through the env variable cf['passenv']
+    'passenv': 'PAPASS',                # name of the environment variable for the password
+    'prompt': r'.*>\s+',                # regex for the prompt
 
-    # duration <= time_delay + time_intervals * (iterations - 1)
+    # max duration (? d ? h ? m ? s) <= time_delay + time_interval * (iterations - 1)
 
-    'duration': '5m',            # max duration (? d ? h ? m ? s) <= time_delay + time_interval * (iterations - 1)
-    'iterations': 999,           # max number of iterations
-    'time_delay': 10,            # initial delay in seconds
-    'time_interval': 20,         # interval in seconds between iterations
+    'duration': '0h2m0s',              # max duration in (? d ? h ? m ? s)
+    'iterations': 999,                  # max number of iterations
+    'time_delay': 10,                   # initial delay in seconds
+    'time_interval': 10,                # interval in seconds between iterations
 
-    'cli': 'cli_1',              # currently not used
-    'cli_timeout': 5,            # > 0 or the cli will not expect a prompt
+    'cli_timeout': 5,                   # timeout > 0 or the prompt will not be expected for the next command
+    'tty_size': (200, 40),              # terminal size for screenful CLI output capture
+    'log_buf_size': 99,                 # log buffer size in message count
 
-    'job_dir':  'job-{}',        # job folder
-    'log_file': 'job-{}.log',    # job log
-    'cnf_file': 'cnf-{}.json',   # config dump
-    'cli_file': 'cli-{}.log',    # CLI output
-    'sta_file': 'sta-{}.json',   # stats
-    'sess_file': 'sess.json',    # session data
+    'job_dir':  'job-{}-{}',            # job folder
+    'log_file': 'job-{}.log',           # job log
+    'cnf_file': 'cf-{}.json',           # config dump
+    'cli_file': 'cli-{}.log',           # CLI output
+    'sta_file': 'sta-{}.json',          # stats
+    'ctx_file': 'ctx-{}.json',
 
-    'plot_file': 'p{0}-{1}.png',      # stats plot file names
-    'plot_file_merged': 'p-{0}.png',  # state merged plot file names
+    'plot_file': 'p{0}-{1}.png',        # stats plot file names
+    'plot_file_merged': 'p-{0}.png',    # state merged plot file names
+    'plot_grid_size': (3, 3),           # (rows, columns) for merged summary plots
 
-    'log_buf_size': 99,          # log buffer size in message count
+    'version': '2.1',
 
     'verbose': True,
     'debug': False,
@@ -78,7 +81,8 @@ c1 = [
     # 'show counter global | match proxy_process',                   # target global counters
     # 'show counter global | match session_installed',               #
     # 'show counter global filter delta yes severity warn',
-    'show interface ethernet1/1 | match "bytes received"',
+    'show interface ethernet1/1 | match "received"',
+    # 'show interface ethernet1/1 | match "bytes received"',
     # 'show interface ethernet1/43 | match "bytes received"',
     'show session distribution policy',                              # chassis models
     # 'show interface all | match ^node',                             # pa-7500 cluster
@@ -111,13 +115,31 @@ metrics = {
     'activeUDPSessions':    r'active UDP sessions:\s+(\d+)',
     'allocatedSessions':    r'allocated sessions:\s+(\d+)',
     'connectionRate':       r'connection establish rate:\s+(\d+) cps',
-    'eth1_1BytesReceived':  r'bytes received\s+(\d+)',
-    # 'eth1_43BytesReceived': r'bytes received\s+(\d+)',
+    'eth1BytesReceived':    r'bytes received\s+(\d+)',
+    'eth1PacketsReceived':  r'packets received\s+(\d+)',
     'flow_ctrl':            r'flow_ctrl\s+:\s+(\d+)%',
     'packetRate':           r'Packet rate:\s+(\d+)\/s',
     'sessionTableUtil':     r'Session table utilization:\s+(\d+)%',
+    'systemResources': (    # %Cpu(s): 19.1 us,  2.7 sy,  0.0 ni, 78.2 id,  0.0 wa,  0.0 hi,  0.0 si,  0.0 st
+        r'%Cpu\(s\):\s*'
+        r'([\d.]+)\s+us,\s*'
+        r'([\d.]+)\s+sy,\s*'
+        r'([\d.]+)\s+ni,\s*'
+        r'([\d.]+)\s+id,\s*'
+        r'([\d.]+)\s+wa,\s*'
+        r'([\d.]+)\s+hi,\s*'
+        r'([\d.]+)\s+si,\s*'
+        r'([\d.]+)\s+st',
+        ['us', 'sy', 'ni', 'id', 'wa', 'hi', 'si', 'st'],
+    ),
     'throughputKbps':       r'Throughput:\s+(\d+) kbps',
     # 'vpnIPSecTunnels':      r'Total (\d+) tunnels found',
+}
+
+metrics2 = {  # derived metrics
+    'eth1PacketSizesAverage':   "eth1BytesReceived / eth1PacketsReceived",
+    'systemResourcesBusy':      "systemResources[:, 0] + systemResources[:, 1]",
+    'systemResourcesUsSy':      "100 - systemResources[:, 3]",
 }
 
 dp = {
@@ -132,7 +154,7 @@ dp = {
     'json_file':            "dp.json",
     'plot_file':            "{0}-{1}.png",
     'plot_file_merged':     "dp-{0}.png",
-    'plot_grid_size':       (3, 3),  # (rows, columns) for merged summary plots
+    'plot_grid_size':       cf['plot_grid_size'],  # (3, 3),  # (rows, columns) for merged summary plots
     'skip_first_row':       True,  # address the issue where the most recent data row is incomplete (all low values)
     'skip_dp_names':        ["s1dp0", "s2dp0"],  # currently not used
 }
